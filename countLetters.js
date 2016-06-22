13e3c5395dbb292f0e46c6a74f01dffdbadd6dbc
Josera89 <jramon.zu@gmail.com>
2016-06-22 20:38:02 +0000

    Second commit

--- countLetters.js
+++ countLetters.js
@@ -1,26 +1,23 @@
 function countLetters(words){
     var letterArray = words.split(" ").join("").toLowerCase().split("");
+    //console.log(letterArray);
     var count = 0;
     var obj = {};
-
-
     for(var i = 0; i < letterArray.length; i++){
 
         var c = letterArray[i];
-        //var d = array.push("jose");
+
         if(obj[c]== undefined){
             var array = [];
+            array.push(i);
+            obj[c]= array;
 
-            if(array.length < 2){
-                array.push(i);
-                obj[c] = array;
-            }
-        }
-        else{
+            //obj[c] = [i]
+        }else{
             obj[c].push(i);
         }
-
     }
     return obj;
 }
 console.log(countLetters("lighthouse in the house"));
+
